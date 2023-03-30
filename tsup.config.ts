9b9c402362600252b238c12c5a2df5c33bae8023
A7mooz <teaming04@gmail.com>
2023-03-30 19:12:57 +0300

    chore: simplified building

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,40 +1,13 @@
-import { defineConfig, Options } from 'tsup';
+import { defineConfig } from 'tsup';
 
-const shared = {
-	entry: ['src/index.ts'],
-	platform: 'node',
+export default defineConfig({
 	clean: true,
+	entry: ['src/index.ts'],
+	format: ['esm', 'cjs'],
+	platform: 'node',
+	target: 'node16',
+	external: ['discord.js', 'mongoose'],
+	treeshake: true,
 	minify: true,
-} satisfies Options;
-
-export default defineConfig([
-	{
-		format: 'esm',
-		target: 'node16',
-		outDir: './dist/esm',
-		treeshake: true,
-		outExtension() {
-			return {
-				js: '.mjs',
-			};
-		},
-		...shared,
-	},
-	{
-		format: 'cjs',
-		splitting: false,
-		target: 'node16',
-		outDir: './dist/cjs',
-		outExtension() {
-			return {
-				js: '.cjs',
-			};
-		},
-		...shared,
-	},
-	{
-		outDir: './dist/types',
-		dts: { only: true },
-		...shared,
-	},
-]);
+	splitting: false
+});

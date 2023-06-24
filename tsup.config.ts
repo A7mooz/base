e6ea33c01a545180c6d1f875989451f8e050f3ce
A7mooz <teaming04@gmail.com>
2023-06-24 05:41:03 +0300

    refactor: changed prettier rules

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	clean: true,
-	entry: ['src/index.ts'],
-	format: ['esm', 'cjs'],
-	platform: 'node',
-	target: 'node16',
-	external: ['discord.js', 'mongoose'],
-	treeshake: true,
-	minify: true,
-	splitting: false,
+    clean: true,
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    platform: 'node',
+    target: 'node16',
+    external: ['discord.js', 'mongoose'],
+    treeshake: true,
+    minify: false,
+    splitting: false,
 });
